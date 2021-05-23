c7f5a2fd4d131cc2cf2de346d3650fe719455917
mwong770 <mwong770@gmail.com>
2021-05-23 19:48:25 -0400

    adds show answer listener

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
     private static final String EXTRA_ANSWER_IS_TRUE="com.example.geoquiz.answer_is_true";
 
     private boolean mAnswerIsTrue;
+    private TextView mAnswerTextView;
+    private Button mShowAnswerButton;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -23,5 +28,17 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+        mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if (mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                } else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
